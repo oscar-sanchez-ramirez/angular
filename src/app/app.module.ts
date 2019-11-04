d866b5b2109baf800da7c66ab03b37d4b8bed8be
oscar <oscar_sr_609@hotmail.com>
2019-11-04 14:29:42 -0600

    navegador y carusel funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CaruselComponent } from './carusel/carusel.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+
+const routes: Routes = [
+  { path: 'quienes', component: QuienesSomosComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CaruselComponent,
+    QuienesSomosComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    CarouselModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
